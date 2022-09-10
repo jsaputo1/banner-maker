42269e900edb7b945e55b11dacf4d341c6624943
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2022-09-10 18:58:44 -0400

    initial js without image upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
-console.log('working');
+$(document).ready(function () {
+    const generateRating = (rating) => {
+        for (let i = 0; i < rating; i++) {
+            $('#stars').append(
+                `<img src="star-solid.svg" />`
+            );
+        }
+    };
+
+    const generateGenres = (input) => {
+        const genres = [input.split(',')];
+        genres[0].forEach(genre => {
+            $('#category-wrapper').append(
+                `<span class="category">${genre}</span>`
+            );
+        });
+
+    };
+
+    $("#submit").on("click", function () {
+        $('.banner-wrap').empty();
+        $('.banner-wrap').append(
+            `<div class="d-flex flex-wrap flex-column justify-content-center align-items-center">
+                <figure class="mb-0"></figure>
+                <h1 class="text-uppercase mb-0" id="header">${$('#heading-input').val()}</h1>
+                <h2 class="text-uppercase" id="sub-header">${$('#sub-heading-input').val()}</h2>
+                <div class="stars mb-3" id="stars"></div>
+                <div class="category-wrapper" id="category-wrapper"></div>
+                <hr>
+                <div class="year" id="year">
+                    <span>${$('#year-input').val()}</span>
+                </div>
+            </div>`
+        );
+        generateRating($('#rating-input').val());
+        generateGenres($('#genre-input').val())
+    });
+});
+
