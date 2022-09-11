0ebe83a3829a6ac64821a0a1e9feb51fc1a842d7
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2022-09-10 20:21:56 -0400

    added file upload js

--- js/main.js
+++ js/main.js
@@ -14,26 +14,36 @@ $(document).ready(function () {
                 `<span class="category">${genre}</span>`
             );
         });
-
     };
 
-    $("#submit").on("click", function () {
-        $('.banner-wrap').empty();
-        $('.banner-wrap').append(
-            `<div class="d-flex flex-wrap flex-column justify-content-center align-items-center">
-                <figure class="mb-0"></figure>
-                <h1 class="text-uppercase mb-0" id="header">${$('#heading-input').val()}</h1>
-                <h2 class="text-uppercase" id="sub-header">${$('#sub-heading-input').val()}</h2>
-                <div class="stars mb-3" id="stars"></div>
-                <div class="category-wrapper" id="category-wrapper"></div>
-                <hr>
-                <div class="year" id="year">
-                    <span>${$('#year-input').val()}</span>
-                </div>
-            </div>`
-        );
-        generateRating($('#rating-input').val());
-        generateGenres($('#genre-input').val())
-    });
-});
+    const generateImage = (input) => {
+        $('#display-image').css('background-image', `url(${input})`);
+    };
 
+    $('#file').on("change", function () {
+        const [imageUrl] = this.files;
+        $("#submit").on("click", function () {
+            $('.banner-wrap').removeClass('d-none');
+            $('.banner-wrap').empty();
+            $('.banner-wrap').append(
+                `<div class="d-flex flex-wrap flex-column justify-content-center align-items-center">
+                    <figure class="mb-0" id="display-image"></figure>
+                    <img id="output" width="200" />	            
+                    </figure>
+                    <h1 class="text-uppercase mb-0" id="header">${$('#heading-input').val()}</h1>
+                    <h2 class="text-uppercase" id="sub-header">${$('#sub-heading-input').val()}</h2>
+                    <div class="stars mb-3" id="stars"></div>
+                    <div class="category-wrapper" id="category-wrapper"></div>
+                    <hr>
+                    <div class="year" id="year">
+                        <span>${$('#year-input').val()}</span>
+                    </div>
+                </div>`
+            );
+            generateImage(URL.createObjectURL(imageUrl));
+            generateRating($('#rating-input').val());
+            generateGenres($('#genre-input').val());
+        });
+    });
+
+});
